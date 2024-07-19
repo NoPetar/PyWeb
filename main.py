4370392db47b46533c1e274a2f0d30583baf171e
NoPetar <kozomorap@gmail.com>
2024-07-19 19:01:12 +0200

    Fixed bugs

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from Elements.page import Page
 from Elements.button import Button
 from Elements.anchor import anchor
+from cssProperties import properties
 from c import *
 
 class PyWeb:
@@ -10,9 +11,9 @@ class PyWeb:
         self.pages : list = []
         self.currentPage : Page = None
         if os.path.isdir(f"{path}") == False:
-            os.mkdir(f'{path}')
+            os.mkdir(f"{path}")
     def open(self):
-        os.system(f'cmd /k "start {self.path}/{self.currentPage.name()}"')
+        os.system(f'cmd /k "start {self.path}/{self.currentPage.name()}')
     def CreatePage(self, name, title):
         self.pages.append(Page(file_name = name , code = "" , title=title))
         return Page(file_name = name , code = "" , title=title)
@@ -22,16 +23,12 @@ class PyWeb:
                 self.currentPage = page
     def execute(self):
         for page in self.pages:
-            f = open(f'{self.path}/{page.name()}' , "w")
+            f = open(f"{self.path}/{page.name()}" , "w")
             f.write(HTML + page.code() + END)
             f.close()
 
-    def Button(self, args : dict = {
-        "width" : float,
-        "height" : float,
-        "text" : str
-    }):
-        self.currentPage.code_ += f'<button style = "width : {args["width"]}px; height : {args["height"]}px">{args["text"]}</button>'
+    def Button(self, args = properties):
+        print(args)
         return Button(float(args["width"]) , float(args["height"]), args["text"])
     def Anchor(self, args : dict = {
         "href" : Page,
@@ -40,7 +37,7 @@ class PyWeb:
         self.currentPage.code_ += f'<a href = {args["href"].name()}>{args["text"]}</a>'
         return anchor(args["href"].name() , args["text"])
     def NewLine(self, amount : int = 1):
-        self.currentPage.code_ += '<br>' * amount
-        self.currentPage.code_ += '\n'
+        self.currentPage.code_ += "<br>" * amount
+        self.currentPage.code_ += "\n"
     def UseCSS(self):
-        f = open(f'{self.path}/styles.css' , "w")
+        f = open(f"{self.path}/styles.css" , "w")
--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from main import *
 website = PyWeb("website")
 a = website.CreatePage("a.html", "a")
 h = website.CreatePage("h.html" , "h")
-website.SelectPage('a.html')
+website.SelectPage("a.html")
 website.Button({
     "width" : 100,
     "height" : 100,
@@ -14,7 +14,7 @@ website.Anchor({
     "href" : h,
     "text" : "Click Me"
 })
-website.SelectPage('h.html')
+website.SelectPage("h.html")
 website.Anchor({
     "href" : a,
     "text" : "Click Me"
